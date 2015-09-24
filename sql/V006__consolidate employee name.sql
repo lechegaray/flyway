042f63f0cb1b52dcae2b094ddc76361e7457e211
Luis Echegaray <mrluisechegaray@gmail.com>
2015-09-24 09:22:03 -0400

    modified version 6 of migration to include altering the employee positions view for the name consolidation

--- sql/V006__consolidate employee name.sql
+++ sql/V006__consolidate employee name.sql
@@ -8,3 +8,10 @@ GO
 ALTER TABLE employees DROP column last_name
 ALTER TABLE employees DROP column first_name
 GO
+
+ALTER VIEW employee_positions AS
+SELECT employees.id AS employee_id,
+	name,
+	title
+FROM employees
+	LEFT JOIN titles on employees.title_id = titles.id
